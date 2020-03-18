9a2025f1ad75b936373a1e7307e3cfa4decde922
Tyler Sharp <johntylersharp@gmail.com>
2020-03-18 15:33:19 -0400

    Solved! Had to edit the html a bit <_<

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// your code here
+/* eslint-disable no-undef */
+/* eslint-disable strict */
+
+function main() {
+
+  let userNumber = +document.getElementById('number-choice').value;
+  console.log(userNumber);
+
+  for (let i = 1; i <= userNumber; i++ ) {
+    const printCount = [];
+    if (i % 15 === 0) {
+      printCount.push(`<div class="fizz-buzz-item fizzbuzz">
+          <span>fizzbuzz</span>
+        </div>`);
+    } else if (i %  5 === 0) {
+      printCount.push(`<div class="fizz-buzz-item buzz">
+        <span>buzz</span>
+      </div>`);
+    } else if (i % 3 === 0) {
+      printCount.push(`<div class="fizz-buzz-item fizz">
+          <span>fizz</span>
+        </div>`);
+    } else {
+      printCount.push(`<div class="fizz-buzz-item">
+      <span>${i}</span>
+      </div>`);
+    }
+    console.log(printCount);
+    $('.js-results').append(printCount);
+
+  }
+}
+  
